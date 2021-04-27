92d1e45e0413fa76fef1106a8676d5bc7d0be310
deepanshu6154 <deepanshu.sharma.18400700@gmail.com>
2021-04-28 00:15:16 +0530

    youtube playlist analzser

--- 4.Async_await/Activity/UtubePlaylistAnalyzer.js
+++ 4.Async_await/Activity/UtubePlaylistAnalyzer.js
@@ -1,47 +1,75 @@
-// no of videos
-// Number of views
-// time length of video
-// list of videos - excel
-
 let puppeteer = require("puppeteer");
 let fs = require("fs");
+const { count } = require("console");
+// no of videos 
+// views 
+// watch time -> get 
+// list of videos -> [name, duration]
+// initial page data get 
+// handle -> loader
 
+console.log("Before");
+// let arr=document.querySelectorAll("#stats  .style-scope.ytd-playlist-sidebar-primary-info-renderer")
+// let newarr=[]
+// newarr.push(arr[0].innerText,arr[1].innerText)
+(async function () {
+    try {
+        let browserInstance = await puppeteer.launch({
+            headless: false,
+            defaultViewport: null,
+            args: ["--start-maximized"]
+        });
+        let newPage = await browserInstance.newPage();
+        await newPage.goto(
+            "https://www.youtube.com/playlist?list=PLRBp0Fe2GpgnIh0AiYKh7o7HnYAej-5ph");
+        // evaluate
+        let arr = await newPage.evaluate(consoleFn);
+        let videoCount = arr[0].split(" ")[0];
+        videoCount = Number(videoCount);
+        console.log(arr[0])
+        console.log(arr[1]);
+        let pCurrentVideosCount = await scrolltoBottom(newPage, "#video-title");
+        while (videoCount - 50 > pCurrentVideosCount) {
+            pCurrentVideosCount = await scrolltoBottom(newPage, "#video-title");
+        }
+        // later
+        let timenDurArr = await newPage.evaluate(getStats,
+            "span.style-scope.ytd-thumbnail-overlay-time-status-renderer",
+            "#video-title");
+        // console.table(timenDurArr.length);
+    } catch (err) {
+        console.log(err);
+    }
+})();
 
-// let arr= document.querySelectorAll("#stats .style-scope.ytd-playlist-sidebar-primary-info-renderer");
-// let newArr = [];
-// newArr.push(arr[0].innerText,arr[1].innerText);
-// newArr
-
-
-// let arr = document.querySelectorAll(".style-scope.ytd-thumbnail-overlay-time-status-renderer")
-// let newArr= [];
-// for(let i=0;i<arr.length;i++){
-//     if(i%2==1){
-//         newArr.push(arr[i].innerText)}
-// }
-// newArr
-
-
-(async function launch(){
-    let browserInstance = await puppeteer.launch({
-        headless:false,
-        defaultViewport:null,
-        args :["--startmaximized"]
-    })
-
-    let page = await browserInstance.newPage();
-    await page.goto("https://www.youtube.com/playlist?list=PLRBp0Fe2GpgnIh0AiYKh7o7HnYAej-5ph");
-
-    let numOfviewsandVideos = await page.evaluate(viewsAndVideos,"#stats .style-scope.ytd-playlist-sidebar-primary-info-renderer");
-    let numOfVideos = numOfviewsandVideos[0].split(" ")[0];
-    numOfVideos = Number(numOfVideos);
-    
-})()
-
-
-async function viewsAndVideos(viewsSelector,timeSelector){
-    let arr= document.querySelectorAll(viewsSelector);
-    let newArr = [];
-    newArr.push(arr[0].innerText,arr[1].innerText);
-    return newArr;
+function consoleFn() {
+    let arr = document.querySelectorAll
+        ("#stats .style-scope.ytd-playlist-sidebar-primary-info-renderer")
+    let newarr = []
+    newarr.push(arr[0].innerText, arr[1].innerText);
+    return newarr;
+} ``
+async function scrolltoBottom(page, title) {
+    function getLengthConsoleFn(title) {
+        window.scrollBy(0, window.innerHeight);
+        let titleElemArr = document.querySelectorAll(title);
+        console.log("titlelength", titleElemArr.length);
+        if (titleElemArr.length == 899) {
+            console.log(titleElemArr);
+        }
+        return titleElemArr.length;
+    }
+    return page.evaluate(getLengthConsoleFn, title);
+}
+// scroll 
+function getStats(durationSelct, title) {
+    let dElemarr = document.querySelectorAll(durationSelct);
+    let titleElemArr = document.querySelectorAll(title);
+    let nameNdurArr = [];
+    for (let i = 0; i < dElemarr.length; i++) {
+        let duration = dElemarr[i].innerText;
+        let title = titleElemArr[i].innerText;
+        nameNdurArr.push({ duration, title });
+    }
+    return nameNdurArr;
 }
