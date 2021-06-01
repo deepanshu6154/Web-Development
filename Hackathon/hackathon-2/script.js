1c3315135d281cbb2f216a11e7044b03aff32e0f
deepanshu6154 <deepanshu.sharma.18400700@gmail.com>
2021-06-01 23:57:48 +0530

    added upto ui

---
+++ Hackathon/hackathon-2/script.js
@@ -0,0 +1,9 @@
+let addPlusBtn = document.querySelector(".fa-plus-circle");
+let incomeContainer = document.querySelector(".income-container");
+addPlusBtn.addEventListener("click",createBox);
+
+
+function createBox(){
+    let div = document.createElement("div");
+    div.innerHTML = ``
+}
